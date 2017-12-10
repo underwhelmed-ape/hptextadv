40056a9275da6558ce7b62b82ae75f3162a62dcd
James U <james.ulyett@gmail.com>
2017-12-10 20:02:45 +0000

    game config
    adding title and libraries

---
+++ game.py
@@ -0,0 +1,11 @@
+# Python Text RPG
+# Underwhelmed Ape
+
+import cmd # help use command line
+import textwrap # wrap text around the console for overflow
+import sys
+import os
+import time
+import random
+
+screen_width = 100
