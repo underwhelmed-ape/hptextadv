ccbb224473b07fedea459e3039d5283d39bbd1c7
James U <james.ulyett@gmail.com>
2017-12-10 22:24:08 +0000

    player setup
    init player with attributes

--- game.py
+++ game.py
@@ -9,3 +9,23 @@ import time
 import random
 
 screen_width = 100
+
+###### Player Setup ######
+
+# establishing state of main character
+# this is a class used to store and initilise values of the player
+class player:
+    def __init__(self):
+        self.name = '' # string of player's name
+        self.hp = 0 # int of hitpoints
+        self.mp = 0 # int of magic points/energy
+        self.status_effects = [] #array
+
+#initilise the player
+
+myPlayer = player()
+
+###### Title Screen ######
+#will allow player to select menu options
+def title_screen_selections():
+    option = input("> ")
