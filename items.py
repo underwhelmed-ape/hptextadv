dbf36cd0466d7ddb9bb2b1493de4156ae2616bfe
James U <james.ulyett@gmail.com>
2018-03-20 21:45:10 +0000

    add weapons class to items

--- items.py
+++ items.py
@@ -1,3 +1,16 @@
+class Weapon:
+    def__init__(self):
+        raise NotImplementedError("Do not create raw Weapon objects.")
+
+    def__str__(self):
+        return self.player_name
+
+class Rock(Weapon):
+    def__init__(self):
+        self.name = "Rock"
+        self.description = "A fist-sized rock, for bludgeoning"
+        self.damage = 5
+
 class HealingPotion(Consumable):
     def__init__(self):
         self.name = "A basic Healing Potion"
