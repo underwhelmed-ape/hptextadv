c4fd1096f43d62b12a1fd521feaf344bba4f4fa1
James U <james.ulyett@gmail.com>
2018-04-11 22:02:09 +0100

    using oop for itema nd weapon classes

--- items.py
+++ items.py
@@ -1,9 +1,6 @@
 class Weapon:
-    def __init__(self):
-        raise NotImplementedError("Do not create raw Weapon objects.")
-
     def __str__(self):
-        return self.player_name
+        return self.name
 
 class Wand(Weapon):
     def __init__(self):
@@ -29,27 +26,33 @@ class Dagger(Weapon):
 #        self.value = 986 # 2G 0S 0K
 
 
-class Purse():
+
+class Item:
+    def __str__(self):
+        return self.name
+
+
+class Purse(Item):
     def __init__(self):
         self.name = "A standard sized purse with all your money"
         self.description = "This purse only holds 15 Galleons"
         self.value = 0
 
 
-class Pocket_Knife():
+class Pocket_Knife(Item):
     def __init__(self):
         self.name = "A small Pocket Knife"
         self.description = "This looks ordinary but looks can be deceiving"
         self.value = 200
 
 
-class Sneakoscope():
+class Sneakoscope(Item):
     def __init__(self):
         self.name = "A basic Sneakoscope"
         self.description = "Useful for detecting untrustworthy people around you"
         self.value = 800
 
-class Potions_kit():
+class Potions_kit(Item):
     def __init__(self):
         self.name = "A portable potions kit for on the go antidotes"
         self.description = "This kit is useful for whipping up emergency antidotes and poisons"
