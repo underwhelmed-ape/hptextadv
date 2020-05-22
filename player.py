33a047f53a8d8f7631d09e213f8f127b828139b3
James Ulyett <underwhelmed-ape@users.noreply.github.com>
2020-05-22 13:50:44 +0100

    add ability to exchange money from wallet

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
-import items
-
+from items import Pocket_Knife, Purse
+from weapons import Wand
+from money_exchange import wizard_money
 
 
 ###### Player Setup ######
@@ -9,38 +10,37 @@ import items
 
 class Player:
     def __init__(self):
-        self.inventory = [items.Wand(),
-                          items.Pocket_Knife(),
-                          items.Rock(),
-                          items.Purse(),
-                          'KB_ticket(0)']
+        self.inventory = [Wand(),
+                          Pocket_Knife(),
+                          Purse()]
         self.name = '' # string of player's name
         self.house = '' # string of player's house in game
         self.subject = ''
         self.hp = 0 # int of hitpoints
         self.mp = 0 # int of magic points/energy
-        self.status_effects = [] #array
-        self.location = 'b2' # string of where player is at the time
+        self.status_effects = []
+        self.location = 'b2'
         self.game_over = False
 
 
     def print_inventory(self):
         print("Inventory: ")
         for item in self.inventory:
-            print("* " + str(item))
-        best_weapon = self.most_important_weapon()
-        print("Your best weapon in your {}".format
-        (best_weapon))
+            print(f'* {item}')
 
-    def most_important_weapon(self):
-        max_damage = 0
-        best_weapon = None
-        for item in self.inventory:
-            try:
-                if item.damage > max_damage:
-                    best_weapon = item
-                    max_damage = item_damage
-            except AttributeError:
-                pass
+    
+    def exchange_money(self, knuts):
+        '''add or subtract money when receiving or spending money'''
+        self.inventory[2].value += knuts
+        print(f'You now have: \n{wizard_money(self.inventory[2].value)} \nin your wallet.')
 
-        return best_weapon
+
+if __name__ == "__main__":
+    from player import Player
+
+    player = Player()
+
+    #player.print_inventory()
+    print(player.inventory[2])
+    
+    player.exchange_money(600)
