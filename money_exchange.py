12f506aeab501bbbab089d7c5e0a3ab062e510f3
James Ulyett <underwhelmed-ape@users.noreply.github.com>
2020-05-22 13:52:38 +0100

    returns on money function

--- money_exchange.py
+++ money_exchange.py
@@ -7,24 +7,26 @@ import math
 
 def wizard_money(knuts):
     if knuts == 0:
-        print("You have no money in your purse")
+        return 'You have no money in your purse'
     elif knuts < 29:
-        print("Knuts: " + str(knuts))
+        return f'Knuts: {knuts}'
     elif knuts < 493:
         sickles = math.floor(knuts / 29)
         remaining = knuts % 29
-        print("Sickles: " + str(sickles))
-        print("Knuts: " + str(remaining))
+        return f'Sickles: {sickles} \nKnuts: {remaining}'
+        
     elif knuts >= 493:
         galleons = math.floor(knuts / 493)
         remaining_knuts = knuts % 493
         sickles = math.floor(remaining_knuts / 29)
         remaining = remaining_knuts % 29
-        print("Galleons: " + str(galleons))
-        print("Sickles: " + str(sickles))
-        print("Knuts: " + str(remaining))
+        return f'  * Galleons: {galleons} \n  * Sickles: {sickles} \n  * Knuts: {remaining}'
+
+
+
+
 
 
 if __name__ == "__main__":
     k = 600
-    wizard_money(k)
+    print(wizard_money(k))
