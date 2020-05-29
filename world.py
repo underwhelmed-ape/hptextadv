b6fa97e14d8be1b02a3504d96f930de9d1f2ed02
James Ulyett <underwhelmed-ape@users.noreply.github.com>
2020-05-29 22:12:38 +0100

    world typos

--- world.py
+++ world.py
@@ -13,40 +13,37 @@ class MapTile:
 class StartTile(MapTile):
     def intro_text(self):
         return '''
-        Welcome to the Ministry of Magic. \n
-        This is where you work. \n
-        '''
+Welcome to the Ministry of Magic. \n
+This is where you work. \n'''
 
 
 class Fireplace(MapTile):
     def intro_text(self):
         return '''
-        You can use Floo Powder here to get to Diagon Alley'''
+You can use Floo Powder here to get to Diagon Alley'''
 
 
 class DiagonAlleyBottom(MapTile):
     def intro_text(self):
         return '''
-        This is the bottom end of Diagon Alley. \n
-        '''
+This is the bottom end of Diagon Alley. \n'''
 
 class DiagonAlleyTop(MapTile):
     def intro_text(self):
         return '''
-        This is the top end of Diagon Alley. \n
-        '''
+This is the top end of Diagon Alley. \n'''
 
 
 class KnockturnAlley(MapTile):
     def intro_text(self):
         return '''
-        This is the crime scene. '''
+This is the crime scene.'''
 
 
 class SecretRoom(MapTile):
     def intro_text(self):
         return '''
-        Congratulations you have solved the crime'''
+Congratulations you have solved the crime'''
 
 
 class PopupPotions(MapTile):
@@ -56,15 +53,15 @@ class PopupPotions(MapTile):
     
     def intro_text(self):
         print('''Trader: \n
-        Welcome to Pop-up Potions, \n
-        Your one stop shop for all your magical needs''')
+Welcome to Pop-up Potions, \n
+Your one stop shop for all your magical needs''')
 
     def trade(self, player):
         print(f'\nHere are my wares! \n')
         for i, item in enumerate(self.trader.inventory, 1):
             print(f'{i}. {item.name}: \n{wizard_money(item.value)}')
         while True:
-            user_input = input("Choose an item number you wish to buy, or press 'Q' to exit my shop \n>")
+            user_input = input("Choose an item number you wish to buy, or press 'Q' to exit my shop \n> ")
             if user_input.lower() == 'q':
                 print('Thank you for visting my shop!')
                 return
@@ -130,211 +127,3 @@ if __name__ == "__main__":
     shop = PopupPotions(1,2)
     shop.intro_text()
     shop.trade(my_player)
-
-
-
-# ###### MAP ######
-# """
-# # player starts at b2 (x)
-#       1   2   3   4
-#     -----------------
-#     |   |   |   |   |  a
-#     -----------------
-#     |   | x |   |   |  b
-#     -----------------
-#     |   |   |   |   |  c
-#     -----------------
-#     |   |   |   |   |  d
-#     -----------------
-
-# """
-
-# # constant variables set-up
-
-# ZONENAME = ''
-# DESCRIPTION = 'description'
-# EXAMINATION = 'examine'
-# SOLVED = 'False'
-# UP = 'up', 'north'
-# DOWN = 'down', 'south'
-# LEFT = 'left', 'west'
-# RIGHT = 'right', 'east'
-
-# solved_places = {'a1': False, 'a2': False, 'a3': False, 'a4': False,
-#                 'b1': False, 'b2': False, 'b3': False, 'b4': False,
-#                 'c1': False, 'c2': False, 'c3': False, 'c4': False,
-#                 'd1': False, 'd2': False, 'd3': False, 'd4': False,
-#                 }
-
-# zonemap = {
-#     'a1': {
-#         ZONENAME: 'Town Market',
-#         DESCRIPTION: 'A thriving market place where you can find many exotic and magical objects',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: '',
-#         DOWN: 'b1',
-#         LEFT: '',
-#         RIGHT: 'a2'
-#     },
-#     'a2': {
-#         ZONENAME: 'Town Entrance',
-#         DESCRIPTION: 'This is the main gate to the town',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: '',
-#         DOWN: 'b2',
-#         LEFT: 'a1',
-#         RIGHT: 'a3'
-#     },
-#     'a3': {
-#         ZONENAME: 'Town Square',
-#         DESCRIPTION: 'A great place for a meeting',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: '',
-#         DOWN: 'b3',
-#         LEFT: 'a2',
-#         RIGHT: 'a4'
-#     },
-#     'a4': {
-#         ZONENAME: 'Town Hall',
-#         DESCRIPTION: 'The home of bureaucracy',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: '',
-#         DOWN: 'b4',
-#         LEFT: 'a3',
-#         RIGHT: ''
-#     },
-#     'b1': {
-#         ZONENAME: "",
-#         DESCRIPTION: 'description',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'a1',
-#         DOWN: 'c1',
-#         LEFT: '',
-#         RIGHT: 'b2'
-#     },
-#     'b2': {
-#         ZONENAME: 'Home',
-#         DESCRIPTION: 'This is your home',
-#         EXAMINATION: 'Your home looks as messy as always! You really should clean up.',
-#         SOLVED: 'False',
-#         UP: 'a2',
-#         DOWN: 'c2',
-#         LEFT: 'b1',
-#         RIGHT: 'b3'
-#     },
-#     'b3': {
-#         ZONENAME: "Forest",
-#         DESCRIPTION: '',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'a3',
-#         DOWN: 'c3',
-#         LEFT: 'b2',
-#         RIGHT: 'b4'
-#     },
-#     'b4': {
-#         ZONENAME: "Empty Shack",
-#         DESCRIPTION: 'This house inside the forest was abandoned long ago ... it looks dangerous to enter ',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'a4',
-#         DOWN: 'c4',
-#         LEFT: 'b3',
-#         RIGHT: ''
-#     },
-#     'c1': {
-#         ZONENAME: "The Dragon Tavern",
-#         DESCRIPTION: '',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'b1',
-#         DOWN: 'd1',
-#         LEFT: '',
-#         RIGHT: 'c2'
-#     },
-#     'c2': {
-#         ZONENAME: "",
-#         DESCRIPTION: 'description',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'b2',
-#         DOWN: 'd2',
-#         LEFT: 'c1',
-#         RIGHT: 'c3'
-#     },
-#     'c3': {
-#         ZONENAME: "",
-#         DESCRIPTION: 'description',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'b3',
-#         DOWN: 'd3',
-#         LEFT: 'c2',
-#         RIGHT: 'c4'
-#     },
-#     'c4': {
-#         ZONENAME: "",
-#         DESCRIPTION: 'description',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'b4',
-#         DOWN: 'd4',
-#         LEFT: 'c3',
-#         RIGHT: ''
-#     },
-#     'd1': {
-#         ZONENAME: "The Dragon Tavern",
-#         DESCRIPTION: 'A popular local haunt. Bring your own glasses!',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'c1',
-#         DOWN: '',
-#         LEFT: '',
-#         RIGHT: 'd2'
-#     },
-#     'd2': {
-#         ZONENAME: "",
-#         DESCRIPTION: 'description',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'c2',
-#         DOWN: '',
-#         LEFT: 'd1',
-#         RIGHT: 'd3'
-#     },
-#     'd3': {
-#         ZONENAME: "",
-#         DESCRIPTION: 'description',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'c3',
-#         DOWN: '',
-#         LEFT: 'd2',
-#         RIGHT: 'd4'
-#     },
-#     'd4': {
-#         ZONENAME: "",
-#         DESCRIPTION: 'description',
-#         EXAMINATION: 'examine',
-#         SOLVED: 'False',
-#         UP: 'c4',
-#         DOWN: '',
-#         LEFT: 'd3',
-#         RIGHT: ''
-#     }
-# #    'd4': {
-# #        ZONENAME: "",
-# #        DESCRIPTION: 'description',
-# #        EXAMINATION: 'examine',
-# #        SOLVED: 'False',
-# #        UP: 'up', 'north',
-# #        DOWN: '',
-# #        LEFT: 'left', 'west',
-# #        RIGHT: 'right', 'east'
-# #    }
-# }
