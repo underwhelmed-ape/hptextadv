8cd510f8bee6087d57e173732ce2ab55c43fb3af
James Ulyett <underwhelmed-ape@users.noreply.github.com>
2020-05-29 20:37:15 +0100

    add tests

--- test/test_player_movement.py
+++ test/test_player_movement.py
@@ -8,8 +8,6 @@ def test_player_starts_at_1_2():
     assert player.y == 2
     
 
-
-
 def test_player_moves_north():
     from player import Player
 
@@ -18,5 +16,40 @@ def test_player_moves_north():
 
     player.move_north()
 
+    assert player.x == 1
     assert player.y == 3
+
+
+def test_player_moves_south():
+    from player import Player
+
+    player = Player()
+    print(f'location: ({player.x},{player.y})')
+
+    player.move_south()
+
+    assert player.x == 1
+    assert player.y == 1
+
+def test_player_moves_east():
+    from player import Player
+
+    player = Player()
+    print(f'location: ({player.x},{player.y})')
+
+    player.move_east()
+
+    assert player.x == 2
+    assert player.y == 2
+
+def test_player_moves_west():
+    from player import Player
+
+    player = Player()
+    print(f'location: ({player.x},{player.y})')
+
+    player.move_west()
+
+    assert player.x == 0
+    assert player.y == 2
 
